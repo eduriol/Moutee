383ed6b44f9e2930e0981b4e76447716c1c3c05a
eduriol <eduriol@gmail.com>
2014-05-15 13:50:03 +0200

    First user and guest models

--- Weddings/models.py
+++ Weddings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.Charfield(max_length=20)
+
+class Guest(models.Model):
+    host = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
