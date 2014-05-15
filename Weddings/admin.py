0af2271174fd18176f276a389fbc18d13ee30ebf
eduriol <eduriol@gmail.com>
2014-05-15 21:10:41 +0200

    Admin interface set up

---
+++ Weddings/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Weddings.models import *
+
+admin.site.register(Wedding)
+admin.site.register(Guest)
+admin.site.register(User)
--- Weddings/models.py
+++ Weddings/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 class User(models.Model):
+    def __unicode__(self):
+        return self.username
     username = models.CharField(max_length=20)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
+    sex = models.BooleanField(default=True)
     email = models.CharField(max_length=50)
-    password = models.Charfield(max_length=20)
+    password = models.CharField(max_length=20)
+
+class Wedding(models.Model):
+    def __unicode__(self):
+        return self.id
+    date = models.DateField()
+    user1 = models.ForeignKey(User, related_name = 'wedding_from_user1')
+    user2 = models.ForeignKey(User, related_name = 'wedding_from_user2')
 
 class Guest(models.Model):
+    def __unicode__(self):
+        return self.name + ' ' + self.surname
     host = models.ForeignKey(User)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
