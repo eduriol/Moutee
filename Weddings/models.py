239fdaec32ba0a7c67b3d2ac43e0544c3adc4d1b
eduriol <eduriol@gmail.com>
2014-05-20 22:48:18 +0200

    Admin interface set up

--- Weddings/models.py
+++ Weddings/models.py
@@ -12,7 +12,7 @@ class User(models.Model):
 
 class Wedding(models.Model):
     def __unicode__(self):
-        return self.id
+        return self.id.__str__()
     date = models.DateField()
     user1 = models.ForeignKey(User, related_name = 'wedding_from_user1')
     user2 = models.ForeignKey(User, related_name = 'wedding_from_user2')
