75b16602d8f52001c0ded908bb89adc284c7f1f2
eduriol <eduriol@gmail.com>
2014-05-21 22:18:46 +0200

    Admin interface improved with fields

--- Weddings/admin.py
+++ Weddings/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from Weddings.models import *
 
-admin.site.register(Wedding)
-admin.site.register(Guest)
-admin.site.register(User)
+
+
+class WeddingAdmin(admin.ModelAdmin):
+    fields = ['date', 'user1', 'user2']
+admin.site.register(Wedding, WeddingAdmin)
+
+class GuestAdmin(admin.ModelAdmin):
+    fields = ['host', 'name', 'surname', 'email']
+admin.site.register(Guest, GuestAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username', 'name', 'surname', 'sex', 'email', 'password']
+admin.site.register(User, UserAdmin)
