889e0e51adfcbf2d3d1e957db32f1169120f322d
eduriol <eduriol@gmail.com>
2014-05-24 18:37:04 +0200

    Inline adding of guests from wedding

--- Weddings/admin.py
+++ Weddings/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from Weddings.models import *
 
-
+class GuestInline(admin.StackedInline):
+    model = Guest
+    extra = 10
 
 class WeddingAdmin(admin.ModelAdmin):
-    fields = ['date', 'user1', 'user2']
+    fieldsets = [
+        ('Hosts',   {'fields': ['user1', 'user2']}),
+        ('Date',    {'fields': ['date']}),
+    ]
+    inlines = [GuestInline]
 admin.site.register(Wedding, WeddingAdmin)
-
-class GuestAdmin(admin.ModelAdmin):
-    fields = ['host', 'name', 'surname', 'email']
-admin.site.register(Guest, GuestAdmin)
 
 class UserAdmin(admin.ModelAdmin):
     fields = ['username', 'name', 'surname', 'sex', 'email', 'password']
--- Weddings/models.py
+++ Weddings/models.py
@@ -20,7 +20,7 @@ class Wedding(models.Model):
 class Guest(models.Model):
     def __unicode__(self):
         return self.name + ' ' + self.surname
-    host = models.ForeignKey(User)
+    wedding = models.ForeignKey(Wedding)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
