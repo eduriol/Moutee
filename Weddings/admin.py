4d3d74fe997065e7b869fddbefe9e8bfdaeae2d0
eduriol <eduriol@gmail.com>
2014-06-17 21:44:18 +0200

    changes in the list of weddings in admin

--- Weddings/admin.py
+++ Weddings/admin.py
@@ -11,5 +11,9 @@ class WeddingAdmin(admin.ModelAdmin):
         ('Date',    {'fields': ['date']}),
     ]
     inlines = [GuestInline]
+    list_display = ('id', 'date', 'user1', 'user2')
+    list_filter = ['date']
+    search_fields = ['user1__first_name', 'user1__last_name', 'user2__first_name', 'user2__last_name']
+    date_hierarchy = 'date'
 
 admin.site.register(Wedding, WeddingAdmin)
