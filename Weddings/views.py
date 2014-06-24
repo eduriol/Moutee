c935987a1525a1b043e6ea1ca279f45bcdb49863
eduriol <eduriol@gmail.com>
2014-06-24 21:18:49 +0200

    developing of index view

--- Weddings/views.py
+++ Weddings/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from Weddings.models import Wedding
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Moutee index.")
+    latest_wedding_list = Wedding.objects.all().order_by('-date')[:5]
+    context = {'latest_wedding_list': latest_wedding_list}
+    return render(request, 'weddings/index.html', context)
 
 def detail(request, wedding_id):
     return HttpResponse("You're looking at wedding %s." % wedding_id)
