056a0c8b2217aae52e984aecd48e05372dc586a8
eduriol <eduriol@gmail.com>
2014-08-05 18:48:52 +0200

    Tests completed. Models unicode fixed.

--- Weddings/models.py
+++ Weddings/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 class Wedding(models.Model):
     def __unicode__(self):
-        return self.id.__str__()
+        return 'Wedding of %s and %s' % (self.user1.first_name, self.user2.first_name)
     date = models.DateField()
     user1 = models.ForeignKey(User, related_name = 'wedding_from_user1')
     user2 = models.ForeignKey(User, related_name = 'wedding_from_user2')
 
 class Guest(models.Model):
     def __unicode__(self):
-        return self.id.__str__()
+        return '%s %s' % (self.name, self.surname)
     wedding = models.ForeignKey(Wedding)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
--- Weddings/tests.py
+++ Weddings/tests.py
@@ -5,13 +5,16 @@ from django.utils.timezone import utc
 
 import datetime
 
-from Weddings.models import Wedding
+from Weddings.models import Wedding, Guest
 
 def create_user(username='john_doe', password='john_doe'):
     return User.objects.create_user(username=username, password=password)
 
 def create_wedding(date, user1, user2):
     return Wedding.objects.create(date=date, user1=user1, user2=user2)
+
+def create_guest(wedding, name='John', surname='Doe', email='john_doe@foo.foo'):
+    return Guest.objects.create(wedding=wedding, name=name, surname=surname, email=email)
 
 class WeddingIndexViewTests(TestCase):
 
@@ -56,16 +59,16 @@ class WeddingDetailViewTests(TestCase):
         self.u1 = User.objects.create_user(username='john_doe', password='john_doe')
         self.u2 = User.objects.create_user(username='jane_doe', password='jane_doe')
 
-    def test_detail_view_of_a_non_existent_wedding(self):
+    def test_detail_view_of_a_non_existing_wedding(self):
         """
         The detail view of a wedding that does not exist should return a 404 not found.
         """
         response = self.client.get(reverse('weddings:detail', args=(1,)))
         self.assertEqual(response.status_code, 404)
 
-    def test_detail_view_of_an_existent_wedding(self):
+    def test_detail_view_of_an_existing_wedding(self):
         """
-        The detail view of an existent wedding should display correctly.
+        The detail view of an existing wedding should display correctly.
         """
         w = create_wedding(datetime.datetime.utcnow().replace(tzinfo=utc), self.u1, self.u2)
         response = self.client.get(reverse('weddings:detail', args=(w.id,)))
@@ -74,3 +77,30 @@ class WeddingDetailViewTests(TestCase):
     def tearDown(self):
         self.u1.delete()
         self.u2.delete()
+
+class GuestDetailViewTests(TestCase):
+
+    def setUp(self):
+        self.u1 = User.objects.create_user(username='john_doe', password='john_doe')
+        self.u2 = User.objects.create_user(username='jane_doe', password='jane_doe')
+        self.w = create_wedding(datetime.datetime.utcnow().replace(tzinfo=utc), self.u1, self.u2)
+
+    def test_detail_view_of_a_non_existing_guest(self):
+        """
+        The detail view of a guest that does not exist should return a 404 not found.
+        """
+        response = self.client.get(reverse('weddings:guest', args=(self.w.id, 1,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_of_an_existing_guest(self):
+        """
+        The detail view of an existing guest should display correctly.
+        """
+        g = create_guest(wedding=self.w, name='John', surname='Doe', email='john_doe@foo.foo')
+        response = self.client.get(reverse('weddings:guest', args=(self.w.id, g.id,)))
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.u1.delete()
+        self.u2.delete()
+        self.w.delete()
